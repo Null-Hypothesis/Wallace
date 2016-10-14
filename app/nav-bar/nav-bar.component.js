f44f564856adca36819f18aff151c427d54caaef
czy941030 <carbon941030@gmail.com>
2016-10-14 11:07:32 +0800

    Add first component nav-bar

---
+++ app/nav-bar/nav-bar.component.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Register `navBar` component, along with its associated controller and template
+angular.module('myApp.navBar').
+
+component('navBar', {
+  templateUrl: 'nav-bar/nav-bar.template.html',
+  controller:
+    function NavBarController() {
+      //
+    }
+});
