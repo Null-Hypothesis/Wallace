6aaffb38e421cbb203ff316879be992877a38422
czy941030 <carbon941030@gmail.com>
2016-10-24 19:13:43 +0800

    view-post added

--- app/posts/view-post.controller.js
+++ app/posts/view-post.controller.js
@@ -2,9 +2,9 @@
 
 angular.module('myApp.posts')
 
-.controller('myApp.posts.viewPost', ['$routeParams', '$rootScope', 'myApp.courses.courseTagsService',
+.controller('myApp.posts.viewPost', ['$routeParams', '$rootScope', '$sce', 'myApp.courses.courseTagsService',
   'myApp.courses.coursesService', 'myApp.posts.postTagsService', 'myApp.posts.postsService',
-function($routeParams, $rootScope, courseTagsService, coursesService, postTagsService, postsService) {
+function($routeParams, $rootScope, $sce, courseTagsService, coursesService, postTagsService, postsService) {
   var self = this;
 
   $rootScope.$on('Course tags loaded', function(event, courseTags) {
@@ -29,5 +29,9 @@ function($routeParams, $rootScope, courseTagsService, coursesService, postTagsSe
 
   self.postId = parseInt($routeParams.postId);
   self.post = postsService.getPostById(self.postId);
+
+  self.renderHtml = function(htmlCode) {
+    return $sce.trustAsHtml(htmlCode);
+  }
 
 }]);
