78fb2a3ab6cc5abf9d45b47d380d98d472abec33
czy941030 <carbon941030@gmail.com>
2016-12-16 15:04:54 +0800

    message component refactory

---
+++ app/user/favorites.service.js
@@ -0,0 +1,21 @@
+'use strict';
+
+angular.module('myApp.user')
+.factory('myApp.user.favoritesService', [
+function() {
+  var service = {};
+
+  service.listAllFavorites = function(userId) {
+    //
+  }
+
+  service.like = function(data) {
+    //
+  };
+
+  service.unlike = function(data) {
+    //
+  };
+
+  return service;
+}]);
---
+++ app/user/message.component.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.module('myApp.user')
+
+.component('message', {
+  templateUrl: 'user/message.template.html',
+  controller: 'myApp.user.message',
+  controllerAs: 'message',
+  bindings: {
+    replyId: '<'
+  }
+});
